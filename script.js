698da9382e384687b181cf953426e0bf46c27d63
Yash Kumar <85291913+yashsaini7060@users.noreply.github.com>
2023-11-29 00:37:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -16,8 +16,17 @@ const library = [
   },
 ];
 
+
 const numberOfBooksRead = () => {
   // write your code here
+	let count =0;
+	for(book in library){
+		if(library[book].readingStatus===true){
+			count++;
+		}
+        // console.log()
+	}
+	return count;
 };
 
 // Do not change the code below
